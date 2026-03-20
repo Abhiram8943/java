94193bbb7a30f356adfbd3b2bb0c99feb0c3f9ac
Abhiram <abhirampariyapurath@gmail.com>
2026-03-20 13:36:33 +0530

    Oops-program-with-function-calls

---
+++ src/JavaNewPackage/Oops2Hello.java
@@ -0,0 +1,22 @@
+package JavaNewPackage;
+
+public class Oops2Hello {
+    public static void main(String[] args)
+    {
+        //make object
+        Oops2Sample s1= new Oops2Sample();
+        Oops2Sample s2= new Oops2Sample();
+
+        s1.a=100;
+        s1.b=150;
+        s2.a=200;
+        s2.b=250;
+
+
+        s1.display();
+        
+        s2.display();
+
+    }
+
+}
---
+++ src/JavaNewPackage/Oops2Sample.java
@@ -0,0 +1,12 @@
+package JavaNewPackage;
+
+public class Oops2Sample {
+    int a;
+    int b;
+
+    void display()
+    {
+        System.out.println("a="+a+" b="+b);
+
+    }
+}
